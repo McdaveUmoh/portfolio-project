b6cda20c0f15a2b72ad631b07799d976ab0df27c
Mcdave Umoh <umohmcdave@gmail.com>
2022-08-18 21:44:38 +0100

    social handle

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -9,8 +9,8 @@ function aboutMe() {
         <div className="About">
             <h4 className="spacing">About Me</h4>
             <p className="spacing">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            <img src={twitter} alt="linkedin img"/>
-            <img src={linkedin} alt="twitter img"/>
+            <img src={twitter} className='pic' alt="linkedin img"/> &nbsp;&nbsp;
+            <img src={linkedin} className='pic' alt="twitter img"/>
         </div>
         <div className='About'></div>
       </div>
